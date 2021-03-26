36d7f466a472dd1d47e029966792dba819c05767
Carlos Andrés Charris S <darkklitos@gmail.com>
2021-03-26 18:14:37 -0500

    [add] servicio

---
+++ src/main/java/com/gdaimon/services/UserService.java
@@ -0,0 +1,29 @@
+package com.gdaimon.services;
+
+import com.gdaimon.models.User;
+import com.gdaimon.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+}
